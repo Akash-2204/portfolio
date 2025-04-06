5805f65e9a703175767f91380b799b82c98f9ac3
Akash <akashthirumuruganantham@gmail.com>
2025-04-05 20:39:20 -0500

    Update contactInfo.tsx

--- src/pages/contactInfo/contactInfo.tsx
+++ src/pages/contactInfo/contactInfo.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import styles from "./contactInfo.module.scss";
 import EmailCard from "@/components/emailCard/emailCard";
-import { abort } from "process";
 import { about } from "@/utils/userData";
 
 const ContactInfo = () => {
